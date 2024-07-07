e64e8af528d9c07d4dbe7a576cdeb5cd33ab935b
Nishijama <nathanplociennik@gmail.com>
2024-07-08 01:40:36 +0200

    working on database implementation

---
+++ FastAPI/main.py
@@ -0,0 +1,108 @@
+# packages
+from fastapi import FastAPI, HTTPException, Depends
+from fastapi.middleware.cors import CORSMiddleware
+from typing import Optional, Annotated, List
+from sqlalchemy.orm import Session
+from pydantic import BaseModel
+import json
+
+# local files
+from database import SessionLocal, engine
+import models
+
+app = FastAPI()
+
+# allow origin from the React app
+origins = ['http://localhost:3000']
+app.add_middleware(CORSMiddleware, allow_origins=origins)
+
+
+class Attendee(BaseModel):
+    first_name: str
+    last_name: str
+    family: str
+    tier: int 
+    so: int | None
+    table: int = 0
+
+class UpdateAttendee(BaseModel):
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    family: Optional[str] = None
+    tier: Optional[int] = None
+    so: Optional[int] = None
+    table: Optional[int] = None
+
+class AttendeeModel(Attendee):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class UpdateAttendeeModel(UpdateAttendee):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+# SET UP DATABASE CONNECTION
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# DEPENDENCY INJECTION - TO UNDERSTAND
+db_dependency = Annotated[Session, Depends(get_db)]
+models.Base.metadata.create_all(bind=engine)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/attendees/{id}", response_model=AttendeeModel)
+async def read_attendees(id: int, db: db_dependency, skip: int = 0, limit: int = 100):
+    attendee = db.query(models.Attendee).filter(models.Attendee.id == id).first()
+    if not attendee:
+        raise HTTPException(status_code=404, detail="Can't edit the attendee. No attendee with provided ID.")
+    return attendee
+
+
+@app.put("/attendees/{id}/", response_model=AttendeeModel)
+async def update_attendee(id: int, attendee_update: UpdateAttendeeModel, db: Session = Depends(db_dependency)):
+    db_attendee = db.query(models.Attendee).filter(models.Attendee.id == id).first()
+    
+    if not db_attendee:
+        raise HTTPException(status_code=404, detail="Attendee not found")
+    
+    update_data = attendee_update.dict(exclude_unset=True)
+    
+    for key, value in update_data.items():
+        setattr(db_attendee, key, value)
+    
+    db.commit()
+    db.refresh(db_attendee)
+    
+    return db_attendee
+
+@app.get("/attendees/", response_model=List[AttendeeModel])
+async def read_attendees(db: db_dependency, skip: int = 0, limit: int = 100):
+    attendees = db.query(models.Attendee).offset(skip).limit(limit).all()
+    return attendees
+
+@app.post("/attendees/", response_model=AttendeeModel)
+async def create_attendees(attendee: AttendeeModel, db: db_dependency):
+    db_attendee = models.Attendee(**attendee.model_dump())
+    db.add(db_attendee)
+    db.commit()
+    db.refresh(db_attendee)
+    return db_attendee
+
+
+# @app.delete("/delete_attendee/{id}")
+# def delete_attendee(id):
+#     if id not in attendees.keys():
+#         return {"Error": "Can't delete the attendee. No attendee with provided ID."}
+#     attendees.pop(id)
+#     return attendees
---
+++ FastAPI/models.py
@@ -0,0 +1,13 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, Boolean
+
+class Attendee(Base):
+    __tablename__='attendees'
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    family = Column(String)
+    tier = Column(Integer)
+    table = Column(Integer)
+    so = Column(Integer)
+    attending = Column(Boolean)
